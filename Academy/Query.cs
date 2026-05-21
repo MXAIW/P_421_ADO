89d2526a14e5b340196b52823a0ba70e3946d574
MXAIW <202803751+MXAIW@users.noreply.github.com>
2026-05-21 19:22:17 +0700

    Class Query DONE

---
+++ Academy/Query.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    class Query
+    {
+        string fields;
+        string tables;
+        string conditions;
+        public Query(string fields, string tables, string conditions = "")
+        {
+            this.fields = fields;
+            this.tables = tables;
+            this.conditions = conditions;
+        }
+        public override string ToString()
+        {
+            string cmd = $"SELECT {fields} FROM {tables} ";
+            if (conditions != "") cmd += $" WHERE {conditions}";
+            return cmd;
+        }
+    }
+}
